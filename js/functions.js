6874faf631d6b1cd18e275d4a867c11424de40fc
quynhnngbs17084 <ngyn.qynh@gmail.com>
2024-10-25 20:31:02 +0700

    page 1

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,33 @@
 (function ($) {
     $(document).ready(function () {
-        var bannerSwiper = new Swiper(".banner__swiper", {
-            loop: true,
-            autoplay: true,
-            pagination: {
-                el: ".banner__swiper .swiper-pagination",
-                clickable: true
-            },
-            navigation: {
-                nextEl: ".banner__swiper .swiper-button-next",
-                prevEl: ".banner__swiper .swiper-button-prev",
-            },
-        });
+        // var bannerSwiper = new Swiper(".banner__swiper", {
+        //     loop: true,
+        //     autoplay: true,
+        //     pagination: {
+        //         el: ".banner__swiper .swiper-pagination",
+        //         clickable: true
+        //     },
+        //     navigation: {
+        //         nextEl: ".banner__swiper .swiper-button-next",
+        //         prevEl: ".banner__swiper .swiper-button-prev",
+        //     },
+        // });
 
         var featuresSwiper = new Swiper(".features__swiper", {
-            slidesPerView: 4,
-            spaceBetween: 20,
+                    breakpoints: {
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 18,
+                        },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 24,
+                    },
+                    1024: {
+                            slidesPerView: 4,
+                    spaceBetween: 30,
+                    }
+                    },
             pagination: {
                 el: ".features__swiper .swiper-pagination",
                 clickable: true
